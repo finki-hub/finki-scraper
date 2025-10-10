06c4fa532965b7eb8f30dcda7ac33cbbbe0578df
Delemangi <milev.stefan@gmail.com>
2025-10-11 00:15:53 +0200

    Remove user agent header

--- src/strategies/DiplomasStrategy.ts
+++ src/strategies/DiplomasStrategy.ts
@@ -88,8 +88,6 @@ export class DiplomasStrategy implements ScraperStrategy {
       credentials: 'include',
       headers: {
         Cookie: cookie,
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
       },
     };
   }
